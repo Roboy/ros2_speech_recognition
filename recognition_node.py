105fd85c5e7a7ee343c955cb0d83dbb4a9400745
missxa <aliona.kharchenko@gmail.com>
2019-04-29 17:36:49 +0200

    a service now

--- recognition_node.py
+++ recognition_node.py
@@ -1,38 +1,50 @@
 #/usr/bin/python3
 import rclpy
 from roboy_cognition_msgs.msg import RecognizedSpeech
+from roboy_cognition_msgs.srv import RecognizeSpeech
 import speech_recognition as sr
 import threading
-from ros2_speech_recognition.recognizer import *
+from recognizer import *
 from time import sleep
 
 BING_KEY = ""
 
+def callback(request, response):
+    global bing, src
+    # msg = RecognizedSpeech()
+    # try:
+    #     msg.source = source.id
+    # except:
+    #     msg.source = -1
+    try:
+        text = bing.stt_with_vad(src)
+        response.text = text
+        # msg.text = text
+        # publisher.publish(msg)
+        print("text: " + text)
+    except sr.UnknownValueError:
+        print("Microsoft Bing Voice Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e,))
+
+    return response
+
 def listener(source, node):
     publisher = node.create_publisher(RecognizedSpeech,\
                                 '/roboy/cognition/speech/recognition')
-
+    global bing, src
+    src = source
     bing = BingVoice(BING_KEY)
-    msg = RecognizedSpeech()
-    try:
-        msg.source = source.id
-    except:
-        msg.source = -1
+    srv = node.create_service(RecognizeSpeech, \
+        '/roboy/cognition/speech/recognition', callback)
 
     # with source as source:
     while rclpy.ok():
-        # input("Press enter to listen again...")
-        #     audio = recognizer.listen(source=source)
-        sleep(1)
-        try:
-            text = bing.stt_with_vad(source)
-            msg.text = text
-            publisher.publish(msg)
-            print("source %i: "%msg.source + text)
-        except sr.UnknownValueError:
-            print("source %i: Microsoft Bing Voice Recognition could not understand audio"%msg.source)
-        except sr.RequestError as e:
-            print("{1}: Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e, msg.source))
+        rclpy.spin_once(node)
+
+    node.destroy_service(srv)
+    node.destroy_publisher(publisher)
+    rclpy.shutdown()
 
 
 def odas_recognition(node):
@@ -60,9 +72,9 @@ def mic_recognition(node):
 def main(args=None):
     rclpy.init()
     node = rclpy.create_node('odas_speech_recognition')
-    # mic_recognition(node)
+    mic_recognition(node)
     # odas_single_channel(node) # requires RPi running odas
-    odas_recognition(node)
+    # odas_recognition(node)
 
 if __name__ == '__main__':
     main()
